06efc2ae4ccca8c97db02e8afb76c7f2ea15800a
hirohikotakezoe <naviestokes7171@gmail.com>
2024-11-20 13:45:14 +0900

    初提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題associative-array</title>
+</head>
+<body>
+    <?php
+        $array_data = ['name' =>'onion','price' => '200', 'weight' =>'160'];
+        echo $array_data;
+
+    ?>
+
+
+</body>
+</html>
