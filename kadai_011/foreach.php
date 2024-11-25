5934bafa41b5a7900c128bf96755c4d983b01bac
hirohikotakezoe <naviestokes7171@gmail.com>
2024-11-25 10:45:47 +0900

    差分

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -8,9 +8,10 @@
 <body>
     <?php
         $array_data = ['名前' =>'玉ねぎ','値段' => '200', '産地' =>'北海道'];
-        foreach(array_data as product_data);
-        echo(product_data.'<br>');
-        
+        foreach($array_data as $key => $value){
+        echo "{$key}:{$value}<br>";
+        }
+
 
     ?>
 
