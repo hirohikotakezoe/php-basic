2d8a52ba71bb27852b61299e339f2dbd11d59df3
hirohikotakezoe <naviestokes7171@gmail.com>
2024-11-25 09:26:29 +0900

    課題

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>課題foreach</title>
+</head>
+<body>
+    <?php
+        $array_data = ['名前' =>'玉ねぎ','値段' => '200', '産地' =>'北海道'];
+        foreach(array_data as product_data);
+        echo(product_data.'<br>');
+        
+
+    ?>
+
+
+</body>
+</html>
