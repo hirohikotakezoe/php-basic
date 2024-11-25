3d756e64de1973628864f4cb349de3de77c5d441
hirohikotakezoe <naviestokes7171@gmail.com>
2024-11-25 12:53:22 +0900

    課題

---
+++ kadai_sort/sort.php
@@ -0,0 +1,17 @@
+$array_data = [15,4,18,23,10];
+
+sort($array_data);
+foreach($array_data as $value){
+    echo"$value/n";
+}
+
+
+
+asort($array_data);
+foreach($array_data as $val){
+    echo"$val/n";
+
+}
+
+
+
